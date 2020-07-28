1efeef65a481e3d5ca569624c3848280068e85af
NullByte75 <66488397+NullByte75@users.noreply.github.com>
2020-07-28 15:59:01 +0200

    Update spy.py

--- spy.py
+++ spy.py
@@ -1,9 +1,6 @@
 import discord
 from discord.ext import commands
 import asyncio
-import os
-import random
-username = os.getlogin()
 token = "" #insert target token
 prefix = ""
 client = discord.Client()
